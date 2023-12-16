b370b0215fca38a85a17fbb668cbbc89fb3cf122
Alexey <113058093+SnowBear7814@users.noreply.github.com>
2023-12-16 09:50:19 +0300

    Update Lab_3_c.c

--- Lab_3_c.c
+++ Lab_3_c.c
@@ -6,10 +6,13 @@ int main() {
     printf("10-num: %d\n", a);
     printf("8-num: %o\n", a);
     printf("16-num: %x\n", a);
+    
     printf("8-num: %o >> 3 == %o\n", a, a >> 3);
     printf("16-num: %x >> 3 == %x\n", a, a >> 3);
+    
     printf("8-num: ~ %o = %o\n", a, ~a);
     printf("16-num: ~ %x = %x\n", a, ~a);
+    
     b = ('%o\n', a);
     c = ('%x\n', a);
     printf("8-num||16-num: %o||%x == %d\n", a, a,b|c);
